1511a09f0c3dabb8bde0d3d1f2736c3dd8a0931c
Ethan Brockway <ebrockway01@gmail.com>
2022-02-01 17:31:51 -0600

    added git user repo based of name enetered in parameter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,12 @@
-var gitUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+var gitUserRepos = function (user) {
+  // format the github API url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
-gitUserRepos();
+gitUserRepos("gstrehlow");
