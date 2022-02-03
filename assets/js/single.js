3e1490defcc1e770c95e9c1899e75954ac942dc0
Ethan Brockway <ebrockway01@gmail.com>
2022-02-03 14:42:23 -0600

    linked html pages and added issues displayed on the second html.

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -14,9 +15,20 @@ var getRepoIssues = function (repo) {
         }
       });
     } else {
-      alert("There was a problem with your request");
+      document.location.replace("./index.html");
     }
   });
+};
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  console.log(repoName);
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");
+  }
 };
 var displayIssues = function (issues) {
   if (issues.length === 0) {
@@ -64,4 +76,4 @@ var displayWarning = function (repo) {
   // append to warning container
   limitWarningEl.appendChild(linkEl);
 };
-getRepoIssues("facebook/react");
+getRepoName();
